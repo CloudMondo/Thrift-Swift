261749cc7dcf6b415dfedaf1d8a7b6c5aea5a67f
Renato Korez <korez.r@gmail.com>
2020-11-19 11:19:52 +0100

    chore: package manifest adds static and dynamic products

--- Package.swift
+++ Package.swift
@@ -1,24 +1,31 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "Thrift"
+    name: "Thrift",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Thrift",
+            targets: ["Thrift"]),
+        .library(
+            name: "ThriftStatic",
+            type: .static,
+            targets: ["Thrift"]),
+        .library(
+            name: "ThriftDynamic",
+            type: .dynamic,
+            targets: ["Thrift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Thrift",
+            dependencies: [],
+            path: "Sources",
+            exclude: ["Info.plist"])
+    ]
 )
